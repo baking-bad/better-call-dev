fd5d9c46bd5f797133d65ce3447c1d67ec9f10dc
Roman Serikov <roman.serikov@yandex.ru>
2019-05-08 18:58:19 +0300

    version 0.0.8

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ Vue.component('loading', VueLoading)
 var app = new Vue({
     el: '#app',
     data: () => ({
-        // address: "KT1HWuhDdbtVQ2S9NAaeEJyCbAF6cMtLcqcc", // AlphaNet
-        // address: "KT1SufMDx6d2tuVe3n6tSYUBNjtV9GgaLgtV", // AlphaNet
-        // address: "KT1FU74GimCeEVRAEZGURb6TWU8jK1N6zFJy", // AlphaNet
-        // address: "KT1HWuhDdbtVQ2S9NAaeEJyCbAF6cMtLcqcc", // AlphaNet
         // address: "KT1ExvG3EjTrvDcAU7EqLNb77agPa5u6KvnY", // MainNet
         address: "",
         typeMap: {},
@@ -24,9 +20,11 @@ var app = new Vue({
             maxPages: 0,
             currentPage: 0,
             tempTxStack: [],
-            data: [],
-            groups: {}
+            data: []
         },
+        groups: {},
+        resultForParameter: {},
+        resultForStorage: {},
         nodeAccountData: {},
         demoAddresses: [{
             net: "alpha",
@@ -40,6 +38,9 @@ var app = new Vue({
         }, {
             net: "alpha",
             address: "KT1HWuhDdbtVQ2S9NAaeEJyCbAF6cMtLcqcc"
+        }, {
+            net: "alpha",
+            address: "KT1QiAJocHUKYN29BegaCnCaSQ9FT2ZXGfuJ"
         }]
     }),
     computed: {
@@ -83,141 +84,40 @@ var app = new Vue({
                 color: "#007bff",
             });
 
-            getPages = async () => {
-                let res = await axios.get(`${this.baseApiURL}/number_operations/${this.address}?type=Transaction`)
-                return res.data;
-            };
-
-            getTransactionData = async () => {
-                let res = await axios.get(`${this.baseApiURL}/operations/${this.address}?type=Transaction&p=${this.txInfo.currentPage}`)
-                return res.data;
-            }
-
-            rebuildTx = (tx) => {
-                let operation = tx["type"]["operations"][0]
-                let dateObj = new Date(operation["timestamp"]);
-
-                return {
-                    "hash": tx["hash"],
-                    "block_hash": tx["block_hash"],
-                    "src": operation["src"]["tz"],
-                    "dst": operation["destination"]["tz"],
-                    "amount": operation["amount"],
-                    "fee": operation["fee"],
-                    "str_parameters": operation["str_parameters"],
-                    "internal": operation["internal"],
-                    "op_level": operation["op_level"],
-                    "decoded_data": null,
-                    "time": dateObj.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' }),
-                    "date": dateObj.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
-                }
-            }
-
-            moveInternalTxToTemp = (data) => {
-                for (let i = data.length - 1; i >= 0; i--) {
-                    if (data[i]["internal"] == true) {
-                        this.txInfo.tempTxStack.push(data[i])
-                    } else {
-                        return data.slice(i)
-                    }
-                }
-            }
-
-            buildTxGroups = (data) => {
-                let res = {};
-                let lastExt = 0;
-
-                for (let i = 0; i < data.length; i++) {
-                    if (data[i]["internal"] == true) {
-                        continue
-                    } else {
-                        let txHash = data[i]["hash"];
-                        res[txHash] = data.slice(lastExt, i + 1).reverse();
-                        lastExt = i + 1;
-                    }
-                }
-                
-                return res
-            }
-
-            // getNodeData = async (block_hash) => {
-            //     let res = await axios.get(`${this.baseNodeApiURL}/${block_hash}/operations/3`)
-            //     return res.data;
-            // }
-
-            getNodeAccountData = async () => {
-                let res = await axios.get(`${this.baseApiURL}/node_account/${this.address}`)
-                return res.data;
-            }
-
-            buildNodeLinks = async () => {
-                let links = [];
-
-                Object.keys(this.txInfo.groups).forEach(function(hash) {
-                    let block_hash = this.txInfo.groups[hash][0]["block_hash"];
-                    links.push(`${this.baseNodeApiURL}/${block_hash}/operations/3`)
-                }, this)
-
-                return links
-            }
-
-            getAllNodeData = async () => {
-                let links = await buildNodeLinks()
-                let promiseArray = links.map( url => axios.get(url) );
-
-                return axios.all( promiseArray )
-                .then((results) => { 
-                    return results.map( el => el.data ).reduce((a, b) => [...a, ...b], [])
-                })
-            }
-
-            this.txInfo.maxPages = Math.ceil(await getPages() / 20)
-            this.txInfo.data = await getTransactionData()
-
-            this.txInfo.data = this.txInfo.data.map(rebuildTx)
-
-            if (this.txInfo.data.length == 20) {
-                this.txInfo.data = moveInternalTxToTemp(this.txInfo.data)
-            }
-
-            this.txInfo.groups = buildTxGroups(this.txInfo.data)
-
-            this.nodeAccountData = await getNodeAccountData()
-
+            this.nodeAccountData = await this.getNodeAccountData();
 
             let code = this.nodeAccountData["script"]["code"];
             let data = this.nodeAccountData["script"]["storage"];
-            let result = {};
-            let result_for_parameter = {};
 
-            code.forEach(function(element) {
-                if (element["prim"] == "storage") {
-                    result = buildSchema(element)
-                } 
-                if (element["prim"] == "parameter") {
-                    result_for_parameter = buildSchema(element)
-                } 
-            });
+            await this.buildSchemas(code)
 
-            this.typeMap = result["type_map"];
-            this.collapsedTree = result["collapsed_tree"];
-            this.decoded_data = decode_data(data, result);
-            this.decoded_schema = decode_schema(result["collapsed_tree"])
+            this.typeMap = this.resultForStorage["type_map"];
+            this.collapsedTree = this.resultForStorage["collapsed_tree"];
+            this.decoded_data = decode_data(data, this.resultForStorage);
+            this.decoded_schema = decode_schema(this.resultForStorage["collapsed_tree"])
+            this.parameterSchema = decode_schema(this.resultForParameter["collapsed_tree"]);
 
-            Object.keys(this.txInfo.groups).forEach(function(hash) {
-                (this.txInfo.groups[hash]).forEach(function(tx) {
+            this.txInfo.maxPages = Math.ceil(await this.getPages() / 20);
+            this.txInfo.data = await this.getTransactionData();
+            this.txInfo.currentPage += 1;
+            this.txInfo.data = this.txInfo.data.map(this.rebuildTx);
+
+            if (this.txInfo.data.length == 20) {
+                this.txInfo.data = this.moveInternalTxToTemp(this.txInfo.data);
+            }
+
+            this.groups = this.buildTxGroups(this.txInfo.data);
+
+            Object.keys(this.groups).forEach(function(hash) {
+                (this.groups[hash]).forEach(function(tx) {
                     let item = JSON.parse(tx["str_parameters"]);
 
-                    tx["decoded_data"] = decode_data(item, result_for_parameter);
+                    tx["decoded_data"] = decode_data(item, this.resultForParameter);
                     tx["result"] = null;
-                })
+                }, this)
             }, this);
 
-            this.parameterSchema = decode_schema(result_for_parameter["collapsed_tree"]);
-
-            
-
-            let final_data = await getAllNodeData()
+            let final_data = await this.getAllNodeData(this.groups)
 
             final_data.forEach(function(node_data) {
                 let big_map_diff = node_data["contents"][0]["metadata"]["operation_result"]["big_map_diff"];
@@ -228,12 +128,113 @@ var app = new Vue({
                     schema["type_map"] = this.typeMap;
                     schema["collapsed_tree"] = this.collapsedTree;
 
-                    this.txInfo.groups[node_data["hash"]][0]["result"] = bigMapDiffDecode(big_map_diff, schema)
+                    this.groups[node_data["hash"]][0]["result"] = bigMapDiffDecode(big_map_diff, schema)
                 }
             }, this)
 
             this.isReady = true;
-            loader.hide()
+            loader.hide();
+        },
+        async initApp() {
+            this.typeMap = {};
+            this.collapsedTree = {};
+            this.isReady = false;
+            this.decoded_data = {};
+            this.decoded_schema = {};
+            this.parameterSchema = {};
+            this.activetab = 1;
+            this.txInfo.maxPages = 0;
+            this.txInfo.currentPage = 0;
+            this.txInfo.tempTxStack = [];
+            this.txInfo.data = [];
+            this.groups = {};
+            this.nodeAccountData = {};
+        },
+        async getNodeAccountData() {
+            let res = await axios.get(`${this.baseApiURL}/node_account/${this.address}`)
+            return res.data;
+        },
+        async buildSchemas(code) {
+            code.forEach(function(element) {
+                if (element["prim"] == "storage") {
+                    this.resultForStorage = buildSchema(element)
+                } 
+                if (element["prim"] == "parameter") {
+                    // result_for_parameter = buildSchema(element)
+                    this.resultForParameter = buildSchema(element);
+                } 
+            }, this);
+        },
+        async getPages() {
+            let res = await axios.get(`${this.baseApiURL}/number_operations/${this.address}?type=Transaction`)
+            return res.data;
+        },
+        async getTransactionData() {
+            let res = await axios.get(`${this.baseApiURL}/operations/${this.address}?type=Transaction&p=${this.txInfo.currentPage}`)
+            return res.data;
+        },
+        rebuildTx(tx) {
+            let operation = tx["type"]["operations"][0]
+            let dateObj = new Date(operation["timestamp"]);
+
+            return {
+                "hash": tx["hash"],
+                "block_hash": tx["block_hash"],
+                "src": operation["src"]["tz"],
+                "dst": operation["destination"]["tz"],
+                "amount": operation["amount"],
+                "fee": operation["fee"],
+                "str_parameters": operation["str_parameters"],
+                "internal": operation["internal"],
+                "op_level": operation["op_level"],
+                "decoded_data": null,
+                "time": dateObj.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' }),
+                "date": dateObj.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
+            }
+        },
+        moveInternalTxToTemp(data) {
+            for (let i = data.length - 1; i >= 0; i--) {
+                if (data[i]["internal"] == true) {
+                    this.txInfo.tempTxStack.push(data[i])
+                } else {
+                    return data.slice(0, i + 1)
+                }
+            }
+        },
+        buildTxGroups(data) {
+            let res = {};
+            let lastExt = 0;
+
+            for (let i = 0; i < data.length; i++) {
+                if (data[i]["internal"] == true) {
+                    continue
+                } else {
+                    let txHash = data[i]["hash"];
+                    res[txHash] = data.slice(lastExt, i + 1).reverse();
+                    lastExt = i + 1;
+                }
+            }
+            
+            return res
+        },
+        async buildNodeLinks(groups) {
+            let links = [];
+
+            Object.keys(groups).forEach(function(hash) {
+                let block_hash = groups[hash][0]["block_hash"];
+                links.push(`${this.baseNodeApiURL}/${block_hash}/operations/3`)
+            }, this)
+
+            return links
+        },
+        async getAllNodeData(groups) {
+            let links = await this.buildNodeLinks(groups);
+            let promiseArray = links.map( url => axios.get(url) );
+
+            return axios.all( promiseArray )
+            .then((results) => { 
+                return results.map( el => el.data ).reduce((a, b) => [...a, ...b], [])
+            })
         },
         formatAddress(address) {
             return address.substr(0,4) + "..." + address.substr(address.length - 4,4)
@@ -252,23 +253,60 @@ var app = new Vue({
             this.address = item["address"]
             this.explore()
         },
-        async initApp() {
-            this.typeMap = {};
-            this.collapsedTree = {};
-            this.isReady = false;
-            this.decoded_data = {};
-            this.decoded_schema = {};
-            this.parameterSchema = {};
+        async loadMore() {
+            let loader = this.$loading.show({
+                container: this.$refs.formContainer,
+                canCancel: false,
+                color: "#007bff",
+            });
+
+            let data = await this.getTransactionData();
+            let currentTxStack = this.txInfo.tempTxStack;
+
+            this.txInfo.currentPage += 1;
+
+            data = data.map(this.rebuildTx);
+
+            if (data.length == 20) {
+                this.txInfo.tempTxStack = [];
+                data = this.moveInternalTxToTemp(data);
+            }
+
+            currentTxStack = currentTxStack.concat(data);
+            let newGroups = this.buildTxGroups(currentTxStack);
+
+            Object.keys(newGroups).forEach(function(hash) {
+                (newGroups[hash]).forEach(function(tx) {
+                    let item = JSON.parse(tx["str_parameters"]);
+
+                    tx["decoded_data"] = decode_data(item, this.resultForParameter);
+                    tx["result"] = null;
+                }, this)
+            }, this);
+
+            let final_data = await this.getAllNodeData(newGroups)
+
+            final_data.forEach(function(node_data) {
+                let big_map_diff = node_data["contents"][0]["metadata"]["operation_result"]["big_map_diff"];
+                
+                if (big_map_diff) {
+                    newGroups[node_data["hash"]][0]["result"] = bigMapDiffDecode(big_map_diff, this.resultForStorage)
+                }
+            }, this)
+
+            this.groups = extend(this.groups, newGroups)
+
+            this.activetab = 2;
             this.activetab = 1;
-            this.txInfo.maxPages = 0;
-            this.txInfo.currentPage = 0;
-            this.txInfo.tempTxStack = [];
-            this.txInfo.data = [];
-            this.txInfo.groups = {};
-            this.nodeAccountData = {};
+            loader.hide();
         }
     }
 })
+
+function extend(obj, src) {
+    Object.keys(src).forEach(function(key) { obj[key] = src[key]; });
+    return obj;
+}
 
 function randomInteger(min, max) {
     var rand = min + Math.random() * (max + 1 - min);
@@ -426,7 +464,9 @@ function decode_data(data, schema, annotations=true, literals=true, rootNode='0'
                     if (type_info['props'] != undefined && annotations) {
                         retRes = {}
                         for (var i = 0; i < res.length; i++) {
-                            retRes[type_info['props'][i]] = res[i]
+                            if (res[i] != undefined) {
+                                retRes[type_info['props'][i]] = res[i]
+                            }  
                         }
                         res = retRes
                     }  
@@ -502,7 +542,9 @@ function decode_literal(node, prim) {
     }
 
     if (prim == "timestamp") {
-        return new Date(Date.parse(value))
+        let ts = parseInt(value) * 1000;
+        let date = new Date(ts);
+        return date.toLocaleString("en-GB")
     } else if (prim == "mutez") {
         return (parseInt(value) / Math.pow(10, 6)).toFixed(6)
     } else if (prim == "bool") {
