430899ca01d807ac98edca4f861a78bbbc2f5e19
Roman Serikov <roman.serikov@yandex.ru>
2019-06-26 18:06:47 +0300

    Added cache

--- src/App.vue
+++ src/App.vue
@@ -47,12 +47,50 @@
 <script>
 import axios from "axios";
 import { bigMapDiffDecode, decodeData, decodeSchema, buildSchema } from "@/app/decode";
+import { demo } from "@/app/demoAddresses";
 
 import Loader from "./components/Loader.vue";
 import NotFound from "./components/NotFound.vue";
 import Results from "./components/Results.vue";
 import NavBar from "./components/NavBar.vue";
 import Landing from "./components/Landing.vue";
+
+axios.interceptors.request.use(
+  request => {
+    let url = request.url;
+    if (url.includes("chains/main/blocks") && !url.includes("head")) {
+      if (localStorage[url] !== undefined) {
+        request.data = JSON.parse(localStorage[url]);
+
+        request.adapter = () => {
+          return Promise.resolve({
+            data: JSON.parse(localStorage[url]),
+            status: request.status,
+            statusText: request.statusText,
+            headers: request.headers,
+            config: request,
+            request: request
+          });
+        };
+      }
+    }
+
+    return request;
+  },
+  error => Promise.reject(error)
+);
+
+axios.interceptors.response.use(
+  response => {
+    let url = response.config.url;
+    if (url.includes("chains/main/blocks") && !url.includes("head")) {
+      localStorage[url] = JSON.stringify(response.data);
+    }
+
+    return response;
+  },
+  error => Promise.reject(error)
+);
 
 export default {
   name: "app",
@@ -87,64 +125,7 @@ export default {
     contractManager: "",
     contractScript: "",
     latestGroup: {},
-    demoAddresses: [
-      {
-        net: "alpha",
-        address: "KT1SufMDx6d2tuVe3n6tSYUBNjtV9GgaLgtV"
-      },
-      {
-        net: "alpha",
-        address: "KT1FU74GimCeEVRAEZGURb6TWU8jK1N6zFJy"
-      },
-      {
-        net: "alpha",
-        address: "KT19iGCL4YrVpT6ezEzbDH37Yxbas8jWQz4s"
-      },
-      {
-        net: "alpha",
-        address: "KT1QiAJocHUKYN29BegaCnCaSQ9FT2ZXGfuJ"
-      },
-      {
-        net: "alpha",
-        address: "KT1HnvV5Z53naoh51jYvPF7w168nW8nfyx5v"
-      },
-      {
-        net: "alpha",
-        address: "KT19yAMFum5MmD99kusQiCBGpTEVC1B52f9Q"
-      },
-      {
-        net: "alpha",
-        address: "KT1Sefu81jFomBUTiJgK6VvCyY5rGrkhPszt"
-      },
-      {
-        net: "alpha",
-        address: "KT1TpKkwKzGwMrWrGnPp9KixhraD2dtE5wE5"
-      },
-      {
-        net: "alpha",
-        address: "KT1P3j1VonQytW3b2SzCnGVpjdf3oWajM79E"
-      },
-      {
-        net: "alpha",
-        address: "KT1XtauF2tnmAKBzbLA2gNoMji9zSzSyYq9w"
-      },
-      {
-        net: "alpha",
-        address: "KT1Qx7PRNAVHgam1qb2MuJohggnSdHTeBWyc"
-      },
-      {
-        net: "main",
-        address: "KT1Q1kfbvzteafLvnGz92DGvkdypXfTGfEA3"
-      },
-      {
-        net: "main",
-        address: "KT1BvVxWM6cjFuJNet4R9m64VDCN2iMvjuGE"
-      },
-      {
-        net: "main",
-        address: "KT1GgUJwMQoFayRYNwamRAYCvHBLzgorLoGo"
-      }
-    ]
+    demoAddresses: demo
   }),
   computed: {
     baseApiURL() {
@@ -464,7 +445,7 @@ export default {
           group.storageSize = currentStorageSize;
         }
       }
-      
+
       let validTezaurus = this.buildValidTezaurus(this.decoded_data, miniTezaurus);
       this.decoded_data = this.mergeTezaurusToStorage(this.decoded_data, validTezaurus);
       return groups;
@@ -476,7 +457,10 @@ export default {
         current = decodedData[key];
       }
 
-      let whiteListKeys = Object.keys(current);
+      let whiteListKeys = [];
+      if (current !== null || current !== undefined) {
+        whiteListKeys = Object.keys(current);
+      }
       let validTezaurus = {};
 
       Object.keys(miniTezaurus).forEach(function(key) {
