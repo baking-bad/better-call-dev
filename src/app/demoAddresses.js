b26c783ad34a22ff77c1ccb69b1403e98331d3cd
Roman Serikov <roman.serikov@yandex.ru>
2019-06-26 18:06:47 +0300

    Added cache

---
+++ src/app/demoAddresses.js
@@ -0,0 +1,58 @@
+export const demo = [
+  {
+    net: "alpha",
+    address: "KT1SufMDx6d2tuVe3n6tSYUBNjtV9GgaLgtV"
+  },
+  {
+    net: "alpha",
+    address: "KT1FU74GimCeEVRAEZGURb6TWU8jK1N6zFJy"
+  },
+  {
+    net: "alpha",
+    address: "KT19iGCL4YrVpT6ezEzbDH37Yxbas8jWQz4s"
+  },
+  {
+    net: "alpha",
+    address: "KT1QiAJocHUKYN29BegaCnCaSQ9FT2ZXGfuJ"
+  },
+  {
+    net: "alpha",
+    address: "KT1HnvV5Z53naoh51jYvPF7w168nW8nfyx5v"
+  },
+  {
+    net: "alpha",
+    address: "KT19yAMFum5MmD99kusQiCBGpTEVC1B52f9Q"
+  },
+  {
+    net: "alpha",
+    address: "KT1Sefu81jFomBUTiJgK6VvCyY5rGrkhPszt"
+  },
+  {
+    net: "alpha",
+    address: "KT1TpKkwKzGwMrWrGnPp9KixhraD2dtE5wE5"
+  },
+  {
+    net: "alpha",
+    address: "KT1P3j1VonQytW3b2SzCnGVpjdf3oWajM79E"
+  },
+  {
+    net: "alpha",
+    address: "KT1XtauF2tnmAKBzbLA2gNoMji9zSzSyYq9w"
+  },
+  {
+    net: "alpha",
+    address: "KT1Qx7PRNAVHgam1qb2MuJohggnSdHTeBWyc"
+  },
+  {
+    net: "main",
+    address: "KT1Q1kfbvzteafLvnGz92DGvkdypXfTGfEA3"
+  },
+  {
+    net: "main",
+    address: "KT1BvVxWM6cjFuJNet4R9m64VDCN2iMvjuGE"
+  },
+  {
+    net: "main",
+    address: "KT1GgUJwMQoFayRYNwamRAYCvHBLzgorLoGo"
+  }
+]
